6737494c338e7ddd4eddccf86615f572000f6515
Hosam124 <hosamelellity121@gmail.com>
2023-02-21 17:01:01 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-
+/*
+ * main - main function
+ * _putchar :print str
+ *Always return 0
+ */
 
 int main(void)
 
 {
-	printf("%s\n", "_putchar");
+	printf("%s\n", "i_putchar");
 	return (0);
 }
